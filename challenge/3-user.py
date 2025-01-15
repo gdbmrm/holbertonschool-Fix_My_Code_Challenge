0c258dd93b4ef4d152a744fbed92a8eed05db722
gdbmrm <>
2025-01-15 14:18:28 +0000

    task 3

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -13,14 +13,13 @@ class User():
     - password: private string hash in MD5
     """
 
-    __password = None
-
     def __init__(self):
         """
         Initialize a new user:
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
+        self.__password = None
 
     @property
     def password(self):
@@ -40,7 +39,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -53,9 +52,9 @@ class User():
         if pwd is None or type(pwd) is not str:
             return False
         if self.__password is None:
+            print("attribut")
             return False
         return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
-
 
 if __name__ == '__main__':
     print("Test User")
@@ -88,7 +87,7 @@ if __name__ == '__main__':
         print("is_valid_password should return True if it's the right \
 password")
 
-    if user_1.is_valid_password("Fakepwd"):
+    """if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right \
 password")
 
@@ -101,3 +100,4 @@ password")
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
 before")
+"""
