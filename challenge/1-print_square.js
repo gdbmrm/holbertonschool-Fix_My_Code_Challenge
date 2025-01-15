fce624d12ec32609dad1e9f80ebc4ffefea91acd
gdbmrm <>
2025-01-15 13:42:02 +0000

    task 1 correction

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -10,10 +10,10 @@ if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(8)
+    process.exit(1)
 }
 
-const size = parseInt(process.argv[2])
+const size = parseInt(process.argv[2], 10)
 
 if(!(Number.isInteger(size))){
     throw new Error("Passed value is not a number");
